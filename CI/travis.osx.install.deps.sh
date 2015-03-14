1cc9529bbaefc7760260a77e022b17788c2b88d6
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-03-14 15:07:33 +0200

    Fixup! Specify output file.

--- CI/travis.osx.install.deps.sh
+++ CI/travis.osx.install.deps.sh
@@ -6,5 +6,5 @@ MONO_VER=3.6.0
 brew update
 which cmake || brew install cmake
 
-wget "https://www.dropbox.com/s/c7zo5cldk7xylk0/MonoFramework-MDK-4.0.0.62.macos10.xamarin.x86.pkg?dl=1"
+wget "https://www.dropbox.com/s/c7zo5cldk7xylk0/MonoFramework-MDK-4.0.0.62.macos10.xamarin.x86.pkg?dl=1" -O "MonoFramework-MDK-4.0.0.62.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-4.0.0.62.macos10.xamarin.x86.pkg" -target /
