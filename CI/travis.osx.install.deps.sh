b88b7195353197158d59b93f0712854025c06ac0
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-03-16 10:59:44 +0200

    Fixup! Correct path.

--- CI/travis.osx.install.deps.sh
+++ CI/travis.osx.install.deps.sh
@@ -7,4 +7,4 @@ brew update
 which cmake || brew install cmake
 
 wget "https://www.dropbox.com/s/zca1sfy7dic8apa/MonoFramework-MDK-4.0.0-therzok.macos10.xamarin.x86.pkg?dl=1" -O "MonoFramework-MDK-4.0.0-therzok.macos10.xamarin.x86.pkg"
-sudo installer -pkg "MonoFramework-MDK-4.0.0.therzok.macos10.xamarin.x86.pkg" -target /
+sudo installer -pkg "MonoFramework-MDK-4.0.0-therzok.macos10.xamarin.x86.pkg" -target /
